a7f56e689ecf9136a3c26bb46bac438c081a18da
Cessabit <sherastrokers123@yandex.ru>
2022-11-19 12:49:35 +0300

    upd

---
+++ codewars copy 25/index.js
@@ -0,0 +1,50 @@
+//Counting sheep...
+
+/*
+    Рассмотрим массив/список овец, где некоторые овцы могут отсутствовать на своем месте. Нам нужна функция, которая подсчитывает количество овец, присутствующих в массиве (истинное означает настоящее).
+Например,
+
+[Правда, правда, правда, ложь,
+
+Правда, правда, правда, правда,
+
+Правда, ложь, правда, ложь,
+
+True, false, false, true,
+
+Правда, правда, правда, правда,
+
+False, false, true, true]
+
+Правильный ответ будет 17.
+
+Подсказка: Не забудьте проверить наличие плохих значений, таких как null/undefined
+
+*/
+
+
+function countSheeps(arrayOfSheep) {
+
+}
+
+// let result = [];
+// function countSheeps(arrayOfSheep) {
+//     for (let i = 0; i < arrayOfSheep.length; i++) {
+//         result = result + i;
+//     }
+//     return result;
+// }
+
+// console.log(countSheeps([2, 2, 3, 3, 2, 2]));
+
+/*let result = '';
+function setCount(num) {
+    for (let i = 1; i <= num; i++) {
+        result = result + i + ' ';
+    }
+    return result;
+}
+console.log(setCount(400)); */
+
+
+
